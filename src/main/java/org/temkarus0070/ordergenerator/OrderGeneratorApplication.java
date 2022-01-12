ca734c1ceaf42c38374598ffc67f55c18f3e8748
temkarus0070 <pretkovartyom@gmail.com>
2022-01-12 09:27:46 +0300

    add swagger

--- src/main/java/org/temkarus0070/ordergenerator/OrderGeneratorApplication.java
+++ src/main/java/org/temkarus0070/ordergenerator/OrderGeneratorApplication.java
@@ -1,9 +1,12 @@
 package org.temkarus0070.ordergenerator;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(description = "${project.description}", title = "${project.title}"))
 public class OrderGeneratorApplication {
 
     public static void main(String[] args) {
