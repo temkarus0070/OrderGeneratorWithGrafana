a8fd113ba9b538a9a5c036d6251972aacec08b6e
temkarus0070 <pretkovartyom@gmail.com>
2022-02-05 15:21:47 +0300

    add grafana and logger

--- src/main/java/org/temkarus0070/ordergenerator/controllers/OrderGeneratorController.java
+++ src/main/java/org/temkarus0070/ordergenerator/controllers/OrderGeneratorController.java
@@ -2,6 +2,8 @@ package org.temkarus0070.ordergenerator.controllers;
 
 
 import io.swagger.v3.oas.annotations.Operation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +14,8 @@ import org.temkarus0070.ordergenerator.services.OrderGenerator;
 @RestController
 @RequestMapping(path = "/order")
 public class OrderGeneratorController {
+
+    Logger logger = LoggerFactory.getLogger(OrderGeneratorController.class);
     private OrderGenerator orderGenerator;
 
     @Autowired
@@ -23,7 +27,13 @@ public class OrderGeneratorController {
     @Operation(description = "get order with fake data")
     @GetMapping(path = "/generate")
     public Order generateOrder() {
-        return orderGenerator.generateOrder();
+        logger.info("generate order");
+        Order order = orderGenerator.generateOrder();
+        if (order == null) {
+            logger.error("order haven't been generated");
+        } else
+            logger.info("return generated order");
+        return order;
     }
 
 }
--- src/main/java/org/temkarus0070/ordergenerator/services/OrderGenerator.java
+++ src/main/java/org/temkarus0070/ordergenerator/services/OrderGenerator.java
@@ -1,6 +1,8 @@
 package org.temkarus0070.ordergenerator.services;
 
 import com.github.javafaker.Faker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.temkarus0070.ordergenerator.models.Good;
 import org.temkarus0070.ordergenerator.models.Order;
@@ -13,14 +15,17 @@ import java.util.Random;
 
 @Service
 public class OrderGenerator {
+    Logger logger = LoggerFactory.getLogger(OrderGenerator.class);
 
     public Order generateOrder() {
+        logger.info("begin of order generation");
         Random random = new Random(new Date().getTime());
         Faker faker = new Faker();
         Order order = new Order();
         order.setClientFIO(faker.name().nameWithMiddle());
         order.setOrderNum(faker.number().numberBetween(1,1000000000));
         int goodsCount = faker.number().numberBetween(1, 100);
+        logger.info("begin to add goods to list");
         List<Good> goods = new ArrayList<>();
         for (int i = 0; i < goodsCount; i++) {
             Good good = new Good();
@@ -47,8 +52,9 @@ public class OrderGenerator {
             good.setSum(good.getPrice() * good.getCount());
             goods.add(good);
         }
+        logger.info("add goods to order");
         order.setGoods(goods);
+        logger.info("return order");
         return order;
-
     }
 }
